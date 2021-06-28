ec33f5659478d150e93de7fcb6cb55e5a5be6234
Chirstian Serrano <sersan.christian@gmail.com>
2021-06-28 14:29:13 -0500

    exam update

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -6,12 +6,22 @@ class User {
     }
 
     static async createUser(name, lastName, email) {
-        return db.push('/users', { name, lastName, email }, false);
+        return db.push('/users', { ['123']: {name, lastName, email} }, false);
     }
 
     static async updateUser(id, name, lastName, email) {
         return db.push(`/users/${id}`, { name, lastName, email }, true);
     }
+
+    static async deleteUser(id) {
+        return db.delete(`/users/${id}`);
+    }
+
+    // * En el registro de usuario encriptar la contraseña y crear un id dinamico
+    // * Crear un endpoint para el login validando la contraseña y verificando si la contraseña exista
+    // * tanto en el registro como en el login devolver un token de verificacion
+    // * Crear un middleware de verificacion de usuario para accesos
+    // * Agregar rol de usuarios para el middleware y que lo tenga el token
 }
 
 module.exports = User;
--- src/models/videogames.models.js
+++ src/models/videogames.models.js
@@ -3,11 +3,15 @@ const { v4 } = require('uuid');
 
 class Videogames {
     // TODO: GET de todos los juegos
-    // TODO: POST un videojuego con los siguientes puntos: nombre, fecha de creacion, categoria y precio
-    // TODO: Get de los juegos mas recientes
+    // TODO: POST un videojuego con los siguientes puntos: nombre, fecha de creacion, categoria, precio y id dinamico
+    // TODO: GET de los juegos mas recientes
     // TODO: GET de los juegos mas caros
+    // TODO: GET de juegos con query en la url (para paginacion)
+    // TODO: GET de juegos con query para busqueda con los datos del videojuego
     // TODO: PUT de un juego con id
     // TODO: DELETE
+
+    // * Con el rol de administrador dar accesos a la creacion, actualizacion y eliminacion de usuario
 }
 
 module.exports = Videogames;
